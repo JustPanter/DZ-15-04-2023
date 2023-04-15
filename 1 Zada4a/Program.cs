376a807aeacdef6f2d66b5484a3f21e0b90acfb9
Panteleev Roman <128892275+JustPanter@users.noreply.github.com>
2023-04-15 20:48:50 +0700

    Add files via upload

---
+++ 1 Zada4a/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.Write("Введите число (не более пятизначного): ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Введеное число {number} является палиндромом");
+  }
+  else Console.WriteLine($"Введеное число {number} не является палиндромом");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите корректное число, в соответствии с запросом");
---
+++ 3 Zada4a/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+int a = 1;
+int i = 1;
+while(i < N + 1)
+{
+    Console.Write($"{a} ");
+    i++;
+    Console.WriteLine(a * a * a);
+    a++;
+}
